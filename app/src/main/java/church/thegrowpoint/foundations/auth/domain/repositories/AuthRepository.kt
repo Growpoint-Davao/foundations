194805e228964cd9d384b2ae6ebced1c1b4ad2c2
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2024-04-26 14:19:57 +0800

    Added auth repository and testing.

--- app/src/main/java/church/thegrowpoint/foundations/auth/domain/repositories/AuthRepository.kt
+++ app/src/main/java/church/thegrowpoint/foundations/auth/domain/repositories/AuthRepository.kt
@@ -4,13 +4,26 @@ import church.thegrowpoint.foundations.auth.domain.models.User
 import com.google.firebase.auth.AuthCredential
 
 /**
- * *AuthRepository*
+ * # AuthRepository
  *
  * The repository interface for the authentication related processes.
  *
  * @property currentUser the current authenticated user.
  */
 interface AuthRepository {
+    /**
+     * *UserResult*
+     *
+     * A simple data class wrapper for a user and possible exception during authentication.
+     *
+     * @property user an instance of a user.
+     * @property exception a possible error exception.
+     */
+    data class UserResult (
+        val user: User? = null,
+        val exception: java.lang.Exception? = null
+    )
+
     val currentUser: User?
 
     /**
@@ -21,29 +34,25 @@ interface AuthRepository {
     /**
      * Registers the user using his / her own [email] and [password].
      * It has callback function for getting the newly registered user and exception error if there's any.
+     *
+     * @return returns [UserResult], a wrapper data class that contains the authenticated user and possible exception.
      */
-    fun register(
-        email: String,
-        password: String,
-        onResult: (user: User?, error: Exception?) -> Unit
-    )
+    suspend fun register(email: String, password: String): UserResult?
 
     /**
      * Signs in user using his / her [email] and [password].
      * It has a callback function for getting the signed user object and exception if something went wrong.
+     *
+     * @return returns [UserResult], a wrapper data class that contains the authenticated user and possible exception.
      */
-    fun signInWithEmailAndPassword(
-        email: String,
-        password: String,
-        onResult: (user: User?, error: Exception?) -> Unit
-    )
+    suspend fun signInWithEmailAndPassword(email: String, password: String): UserResult?
 
     /**
      * Signs using firebase [AuthCredential].
      * It has a callback function for getting the signed user object and exception if something went wrong.
+     * This can be used when supporting Facebook, Google, and other social medial sign-in methods.
+     *
+     * @return returns [UserResult], a wrapper data class that contains the authenticated user and possible exception.
      */
-    fun firebaseSignIn(
-        credential: AuthCredential,
-        onResult: (user: User?, error: Exception?) -> Unit
-    )
+    suspend fun signInWithCredential(credential: AuthCredential): UserResult?
 }
