d91aaa26cfffec2a2278ba879894dc68eaf8dee1
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2024-05-02 23:03:22 +0800

    Added drawer title

--- app/src/main/java/church/thegrowpoint/foundations/modules/content/presentation/FoundationsContent.kt
+++ app/src/main/java/church/thegrowpoint/foundations/modules/content/presentation/FoundationsContent.kt
@@ -1,7 +1,9 @@
 package church.thegrowpoint.foundations.modules.content.presentation
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -16,6 +18,7 @@ import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -25,6 +28,7 @@ import androidx.navigation.compose.rememberNavController
 import church.thegrowpoint.foundations.R
 import church.thegrowpoint.foundations.modules.auth.presentation.AuthViewModel
 import church.thegrowpoint.foundations.ui.composables.CenteredTopAppBar
+import church.thegrowpoint.foundations.ui.composables.GrowpointTitlePanel
 import church.thegrowpoint.foundations.ui.composables.NavigationDrawerItemWithProgress
 import kotlinx.coroutines.launch
 
@@ -41,11 +45,18 @@ fun FoundationsContent(
     ModalNavigationDrawer(
         drawerContent = {
             ModalDrawerSheet {
-                Text(
-                    "Foundations",
-                    modifier = Modifier.padding(16.dp)
-                )
+                Column {
+                    Spacer(modifier = Modifier.height(16.dp))
+                    GrowpointTitlePanel(
+                        modifier = Modifier.padding(16.dp),
+                        title = stringResource(R.string.foundations),
+                        subTitle = stringResource(R.string.established_for_growing),
+                        logo = painterResource(R.drawable.gp_login_logo)
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
                 HorizontalDivider()
+                Spacer(modifier = Modifier.height(8.dp))
                 NavigationDrawerItemWithProgress(
                     title = stringResource(R.string.getting_started),
                     subTitle = stringResource(R.string.introduction)
