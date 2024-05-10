7451c6c24e0fd9658b3eae3970f318402595f8c5
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2024-05-10 18:06:08 +0800

    Fixed the error in build gradle and added provides function in fake module.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ android {
     }
 
      testOptions {
-         packagingOptions {
+         packaging {
              jniLibs {
                  useLegacyPackaging = true
              }
--- app/src/androidTest/java/church/thegrowpoint/foundations/modules/auth/FakeAuthModuleAllMocked.kt
+++ app/src/androidTest/java/church/thegrowpoint/foundations/modules/auth/FakeAuthModuleAllMocked.kt
@@ -9,6 +9,13 @@ import church.thegrowpoint.foundations.modules.auth.domain.usecases.RegisterUser
 import church.thegrowpoint.foundations.modules.auth.domain.usecases.SignInWithEmailAndPassword
 import church.thegrowpoint.foundations.modules.auth.domain.usecases.SignInWithGoogle
 import church.thegrowpoint.foundations.modules.auth.domain.usecases.SignOutUser
+import church.thegrowpoint.foundations.modules.content.data.datasources.AssetsService
+import church.thegrowpoint.foundations.modules.content.data.datasources.AssetsServiceImplementation
+import church.thegrowpoint.foundations.modules.content.data.datasources.ContentService
+import church.thegrowpoint.foundations.modules.content.data.datasources.ContentServiceImplementation
+import church.thegrowpoint.foundations.modules.content.data.repositories.ContentRepositoryImplementation
+import church.thegrowpoint.foundations.modules.content.domain.repositories.ContentRepository
+import church.thegrowpoint.foundations.modules.content.domain.usecases.GetContent
 import com.google.firebase.auth.FirebaseAuth
 import dagger.Module
 import dagger.Provides
@@ -77,5 +84,29 @@ class FakeAuthModuleAllMocked {
     @Provides
     fun provideCoroutineDispatcher(): CoroutineDispatcher {
         return Dispatchers.Default
+    }
+
+    @Provides
+    @Singleton
+    fun provideAssetsService(@ApplicationContext context: Context): AssetsService {
+        return AssetsServiceImplementation(context)
+    }
+
+    @Provides
+    @Singleton
+    fun provideContentService(assetService: AssetsService): ContentService {
+        return ContentServiceImplementation(assetService)
+    }
+
+    @Provides
+    @Singleton
+    fun provideContentRepository(contentService: ContentService): ContentRepository {
+        return ContentRepositoryImplementation(contentService)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGetContent(contentRepository: ContentRepository): GetContent {
+        return GetContent(contentRepository)
     }
 }
