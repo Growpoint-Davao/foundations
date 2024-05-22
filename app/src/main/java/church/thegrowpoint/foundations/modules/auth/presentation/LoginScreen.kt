3aaae9d63301f109b43ab0f061f231fc465b1769
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2024-05-22 22:22:36 +0800

    Polished the registration.

--- app/src/main/java/church/thegrowpoint/foundations/modules/auth/presentation/LoginScreen.kt
+++ app/src/main/java/church/thegrowpoint/foundations/modules/auth/presentation/LoginScreen.kt
@@ -11,19 +11,12 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Visibility
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -31,10 +24,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -45,12 +34,9 @@ import church.thegrowpoint.foundations.modules.Routes
 import church.thegrowpoint.foundations.ui.composables.ActionableDialog
 import church.thegrowpoint.foundations.ui.composables.ClickableLabel
 import church.thegrowpoint.foundations.ui.composables.DialogAction
-import church.thegrowpoint.foundations.ui.composables.ErrorLabel
 import church.thegrowpoint.foundations.ui.composables.LargeButton
-import church.thegrowpoint.foundations.ui.composables.RoundedTextInputField
 import church.thegrowpoint.foundations.ui.composables.WhiteIconButton
 import church.thegrowpoint.foundations.ui.theme.FoundationsTheme
-import church.thegrowpoint.foundations.ui.theme.RoundedShapes
 import church.thegrowpoint.foundations.utils.extensions.validEmail
 import church.thegrowpoint.foundations.utils.extensions.validPasswordLength
 import java.util.Locale
@@ -212,69 +198,6 @@ fun LoginScreen(
             }
         }
     }
-}
-
-@Composable
-fun EmailField(
-    modifier: Modifier = Modifier,
-    value: String = "",
-    onValueChange: (String) -> Unit,
-    isError: Boolean = false
-) {
-    RoundedTextInputField(
-        modifier = modifier,
-        label = stringResource(R.string.email),
-        value = value,
-        onValueChange = onValueChange,
-        keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Email,
-            imeAction = ImeAction.Next
-        ),
-        isError = isError,
-        supportText = stringResource(R.string.please_provide_a_valid_email)
-    )
-}
-
-@Composable
-fun PasswordField(
-    supportingText: String,
-    modifier: Modifier = Modifier,
-    value: String = "",
-    isError: Boolean = false,
-    onValueChange: (String) -> Unit
-) {
-    var showPassword by remember { mutableStateOf(false) }
-
-    OutlinedTextField(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedShapes.large,
-        value = value,
-        onValueChange = onValueChange,
-        isError = isError,
-        supportingText = {
-            if (isError) {
-                ErrorLabel(
-                    modifier = Modifier.fillMaxWidth(),
-                    text = supportingText
-                )
-            }
-        },
-        label = { Text(stringResource(R.string.password)) },
-        placeholder = { Text(text = stringResource(R.string.enter_password)) },
-        trailingIcon = {
-            IconButton(onClick = { showPassword = !showPassword }) {
-                Icon(
-                    Icons.Filled.Visibility,
-                    contentDescription = stringResource(R.string.show_hide_password)
-                )
-            }
-        },
-        visualTransformation = if (showPassword) VisualTransformation.None else PasswordVisualTransformation(),
-        keyboardOptions = KeyboardOptions(
-            keyboardType = KeyboardType.Password,
-            imeAction = ImeAction.Done
-        )
-    )
 }
 
 @Preview(
--- app/src/main/java/church/thegrowpoint/foundations/modules/auth/presentation/RegistrationScreen.kt
+++ app/src/main/java/church/thegrowpoint/foundations/modules/auth/presentation/RegistrationScreen.kt
@@ -1,30 +1,51 @@
 package church.thegrowpoint.foundations.modules.auth.presentation
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.rounded.Save
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import church.thegrowpoint.foundations.R
 import church.thegrowpoint.foundations.ui.composables.CenteredTopAppBar
+import church.thegrowpoint.foundations.ui.theme.RoundedShapes
+import church.thegrowpoint.foundations.utils.extensions.validEmail
+import church.thegrowpoint.foundations.utils.extensions.validPasswordLength
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RegistrationScreen(
+    modifier: Modifier = Modifier,
     authViewModel: AuthViewModel = hiltViewModel(),
     appNavController: NavHostController = rememberNavController(),
 ) {
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    var confirmPassword by rememberSaveable { mutableStateOf("") }
+    var isValidEmail by rememberSaveable { mutableStateOf(true) }
+    var pwLengthValid by rememberSaveable { mutableStateOf(true) }
+    var confirmPwLengthValid by rememberSaveable { mutableStateOf(true) }
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
@@ -39,11 +60,56 @@ fun RegistrationScreen(
         },
         content = { innerPadding ->
             Column(
-                modifier = Modifier
+                modifier = modifier
                     .padding(innerPadding)
-                    .fillMaxSize()
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
             ) {
+                Column (modifier = modifier.padding(horizontal = 32.dp)) {
+                    Spacer(modifier = Modifier.height(32.dp))
+                    EmailField(
+                        value = email,
+                        onValueChange = {
+                            email = it.trim()
+                            isValidEmail = email.validEmail()
+                        },
+                        isError = !isValidEmail
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    PasswordField(
+                        value = password,
+                        onValueChange = {
+                            password = it
+                            pwLengthValid = password.validPasswordLength()
+                        },
+                        imeAction = ImeAction.Next,
+                        isError = !pwLengthValid || (password != confirmPassword),
+                        supportingText = if (!pwLengthValid) stringResource(R.string.password_is_too_short) else stringResource(R.string.passwords_do_not_match)
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    PasswordField(
+                        value = confirmPassword,
+                        label = stringResource(R.string.confirm_password),
+                        onValueChange = {
+                            confirmPassword = it
+                            confirmPwLengthValid = confirmPassword.validPasswordLength()
+                        },
+                        isError = !confirmPwLengthValid,
+                        supportingText = stringResource(R.string.password_is_too_short)
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(shape = RoundedShapes.large, onClick = {
 
+            }) {
+                Icon(
+                    imageVector = Icons.Rounded.Save,
+                    contentDescription = stringResource(R.string.save)
+                )
             }
         }
     )
