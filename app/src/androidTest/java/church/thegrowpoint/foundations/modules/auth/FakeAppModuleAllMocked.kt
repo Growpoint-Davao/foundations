93df9fefaa23a54c745510da924b71c136167c8b
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2024-05-24 15:49:35 +0800

    Upgraded the dependencies

--- app/src/androidTest/java/church/thegrowpoint/foundations/modules/auth/FakeAppModuleAllMocked.kt
+++ app/src/androidTest/java/church/thegrowpoint/foundations/modules/auth/FakeAppModuleAllMocked.kt
@@ -9,11 +9,6 @@ import church.thegrowpoint.foundations.modules.auth.domain.usecases.RegisterUser
 import church.thegrowpoint.foundations.modules.auth.domain.usecases.SignInWithEmailAndPassword
 import church.thegrowpoint.foundations.modules.auth.domain.usecases.SignInWithGoogle
 import church.thegrowpoint.foundations.modules.auth.domain.usecases.SignOutUser
-import church.thegrowpoint.foundations.modules.content.data.datasources.AssetsService
-import church.thegrowpoint.foundations.modules.content.data.datasources.AssetsServiceImplementation
-import church.thegrowpoint.foundations.modules.content.data.datasources.ContentService
-import church.thegrowpoint.foundations.modules.content.data.datasources.ContentServiceImplementation
-import church.thegrowpoint.foundations.modules.content.domain.usecases.GetContent
 import com.google.firebase.auth.FirebaseAuth
 import dagger.Module
 import dagger.Provides
@@ -82,29 +77,5 @@ class FakeAppModuleAllMocked {
     @Provides
     fun provideCoroutineDispatcher(): CoroutineDispatcher {
         return Dispatchers.Default
-    }
-
-    @Provides
-    @Singleton
-    fun provideAssetsService(@ApplicationContext context: Context): AssetsService {
-        return AssetsServiceImplementation(context)
-    }
-
-    @Provides
-    @Singleton
-    fun provideContentService(assetService: AssetsService): ContentService {
-        return ContentServiceImplementation(assetService)
-    }
-
-    @Provides
-    @Singleton
-    fun provideContentRepository(contentService: ContentService): ContentRepository {
-        return ContentRepositoryImplementation(contentService)
-    }
-
-    @Provides
-    @Singleton
-    fun provideGetContent(contentRepository: ContentRepository): GetContent {
-        return GetContent(contentRepository)
     }
 }
