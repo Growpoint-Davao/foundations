45a2c8fb7ffd31c74998b492e14bf02f4d37c349
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2024-05-29 18:29:53 +0800

    Added use cases.

---
+++ app/src/main/java/church/thegrowpoint/foundations/modules/auth/domain/usecases/GetSkipAuthFlow.kt
@@ -0,0 +1,23 @@
+package church.thegrowpoint.foundations.modules.auth.domain.usecases
+
+import church.thegrowpoint.foundations.modules.auth.domain.repositories.AuthRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+/**
+ * # GetSkipAuthFlow
+ *
+ * The use case for retrieving the skip auth flow from the AuthRepository.
+ *
+ * @property authRepository The AuthRepository instance.
+ */
+class GetSkipAuthFlow @Inject constructor(private val authRepository: AuthRepository) {
+    /**
+     * Retrieves the skip auth flow from the AuthRepository.
+     *
+     * @return the flow state for skip auth.
+     */
+    operator fun invoke(): Flow<Int> {
+        return authRepository.getSkipAuthFlow()
+    }
+}
