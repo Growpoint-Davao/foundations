ed426b7b6d1fee285d30011b0c051e2716ad661f
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2024-06-02 20:00:01 +0800

    Added properties.

--- app/src/main/java/church/thegrowpoint/foundations/modules/content/presentation/states/IdentityAnswersUIState.kt
+++ app/src/main/java/church/thegrowpoint/foundations/modules/content/presentation/states/IdentityAnswersUIState.kt
@@ -1,5 +1,19 @@
 package church.thegrowpoint.foundations.modules.content.presentation.states
 
 data class IdentityAnswersUIState(
+    val whatOthersThinkOfMe: Boolean = false,
+    val myWeaknesses: Boolean = false,
+    val myGradePointAverage: Boolean = false,
+    val myPhysicalStrengths: Boolean = false,
+    val whatILookInTheMirror: Boolean = false,
+    val myFriendsIHangoutWith: Boolean = false,
+    val myTalentsAndAbilities: Boolean = false,
+    val myIntelligence: Boolean = false,
+    val whatMyFamilySaysAboutMe: Boolean = false,
+    val myFamilyReputation: Boolean = false,
+    val myAttitude: Boolean = false,
+    val whatGodSaysAboutMe: Boolean = false,
+    val theClothsIWear: Boolean = false,
+    val others: Boolean = false,
     override val answers: HashMap<String, String> = HashMap()
 ) : AnswersUIState(answers)
