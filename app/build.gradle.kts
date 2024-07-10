0ffd6cb32faec041a9b38ebcecdd778cb51d5af0
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2024-07-10 10:57:07 +0800

    Version update.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "church.thegrowpoint.foundations"
         minSdk = 25
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.0.1"
+        versionCode = 3
+        versionName = "0.0.2"
 
         // testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         testInstrumentationRunner = "church.thegrowpoint.foundations.CustomTestRunner"
